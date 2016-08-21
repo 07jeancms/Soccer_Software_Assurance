6e98094f8d638f96d33f8e0f0ea420eb19192dca
Jean Carlo Mata <jean.cms07@gmail.com>
2016-08-20 18:27:26 -0600

    Fourth OpenCV functionality

--- Open_CV/src/App/Video_Processing.java
+++ Open_CV/src/App/Video_Processing.java
@@ -41,32 +41,34 @@ public class Video_Processing {
 		videoName = pVideoName;
 	}
 	
-	public boolean openVideo() throws IOException{
+	/*
+	 * Verify if a video isOpen. It's must be under relative path
+	 * 
+	 * Parameter: -------
+	 */
+	public boolean videoIsOpen()  {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		
-		// Load the video file into the capture
-		VideoCapture capture;
-		
+		boolean isOpen = false;
 		try{
-			capture = new VideoCapture(videoName);
-			//System.out.println(fileName + " is open");
-			return true;
-		}catch(Exception e){
-			System.out.println("Error: Openning" + videoName);
-			return false;
+			// Load the video file into the capture
+			VideoCapture video = new VideoCapture(videoName);
+			
+			if(video.isOpened())
+				isOpen = true;
+			
+		}catch(Exception ex){
+			
+			isOpen = false;
 		}
+		return isOpen;
+		
 	}
 	
-	//======================================================================
-	
-	public static boolean checkFileExists(String filePath) throws IOException {
-		File f = new File(filePath);
-		if(f.exists() && !f.isDirectory()) { 
-		    return true;
-		}
-		return false;
-	}
-	
+	/*
+	 * Enter a video file name using console
+	 * 
+	 * Parameters: -------
+	 */
 	//======================================================================
 	
 	public String readFileName() throws IOException{
@@ -88,25 +90,28 @@ public class Video_Processing {
 	
 	public ArrayList<Mat> readFrames(){
 		
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		//System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		
-		// Load the video file into the capture
-		VideoCapture videoCap = new VideoCapture(videoName);
+		if(videoIsOpen()){
+			// Load the video file into the capture
+			VideoCapture videoCap = new VideoCapture(videoName);
 
-	    double amountOfFrames = videoCap.get(CV_CAP_PROP_FRAME_COUNT); //get the frame count
-	    
-	    ArrayList<Mat> frames = new ArrayList<>();
-	   
-	    for(int actualFrame = 0; actualFrame < amountOfFrames; actualFrame++){
-	    	Mat frame = new Mat();
-	    	videoCap.read(frame);
-	        if (frame.empty()) 
-	        	break;
-	        frames.add(frame);
-	     }
-	    
-	    System.out.println("Amount of frames: " + frames.size());	
-	    return frames;
+		    double amountOfFrames = videoCap.get(CV_CAP_PROP_FRAME_COUNT); //get the frame count
+		    
+		    ArrayList<Mat> frames = new ArrayList<>();
+		   
+		    for(int actualFrame = 0; actualFrame < amountOfFrames; actualFrame++){
+		    	Mat frame = new Mat();
+		    	videoCap.read(frame);
+		        if (frame.empty()) 
+		        	break;
+		        frames.add(frame);
+		     }
+		    
+		    System.out.println("Amount of frames: " + frames.size());	
+		    return frames;
+		}
+		return null;
 	}
 	
 	//======================================================================
@@ -118,16 +123,16 @@ public class Video_Processing {
 	 * Parameters: Mat
 	 * A Mat type (image) to be printed
 	 */
-	public void printImage(Mat m){
+	public void printImage(String pWindowName, Mat pMat){
 	    BufferedImage bufImage = null;
 	    try {
 	        MatOfByte matOfByte = new MatOfByte();
-	        Highgui.imencode(".jpg", m, matOfByte); 
+	        Highgui.imencode(".jpg", pMat, matOfByte); 
 	        byte[] byteArray = matOfByte.toArray();
 	        InputStream in = new ByteArrayInputStream(byteArray);
 	        bufImage = ImageIO.read(in);
 
-	        JFrame frame = new JFrame("Image");
+	        JFrame frame = new JFrame(pWindowName);
 	        frame.getContentPane().setLayout(new FlowLayout());
 	        frame.getContentPane().add(new JLabel(new ImageIcon(bufImage)));
 	        frame.pack();
@@ -144,22 +149,56 @@ public class Video_Processing {
 	/*
 	 * Convert an RGB image to HSV image
 	 * 
-	 * Parameters: ArrayList of mat types to be converted to HSV Arraylist
+	 * Parameters: ArrayList of mat types to be converted to HSV ArrayList
 	 */
 	public ArrayList<Mat> convertToHSV(ArrayList<Mat> pMatArray){
-		int pMatArraySize = pMatArray.size();
-		ArrayList<Mat> arrayListHSV = new ArrayList<>();
-		for(int actualMatCounter = 0; actualMatCounter < pMatArraySize; actualMatCounter++){
-			Mat hsv = new Mat();
-			Mat actualMatElement = pMatArray.get(actualMatCounter);
-		    Imgproc.cvtColor(actualMatElement, hsv, Imgproc.COLOR_RGB2HSV);	
-		    arrayListHSV.add(hsv);
+		if(videoIsOpen()){
+			int pMatArraySize = pMatArray.size();
+			ArrayList<Mat> arrayListHSV = new ArrayList<>();
+			for(int actualMatCounter = 0; actualMatCounter < pMatArraySize; actualMatCounter++){
+				Mat hsv = new Mat();
+				Mat actualMatElement = pMatArray.get(actualMatCounter);
+			    Imgproc.cvtColor(actualMatElement, hsv, Imgproc.COLOR_RGB2HSV);	
+			    arrayListHSV.add(hsv);
+			}
+			return arrayListHSV;
 		}
-		return arrayListHSV;
+		return null;
 	}
 	
 	
 	//======================================================================
+	
+	
+	/*
+	 * Get the H layer from HSV Mat
+	 * 
+	 * Parameters: An ArrayList<Mat> Mat images with HSV format
+	 */
+	public ArrayList<Mat> getHlayer(ArrayList<Mat> pHSVarrayList){
+		int amountOfMats = pHSVarrayList.size();
+		ArrayList<Mat> hArrayList = new ArrayList<>();
+		for(int actualMat = 0; actualMat < amountOfMats; actualMat++){
+			
+			Mat currentMatValue = pHSVarrayList.get(actualMat);
+			
+			// Get V from HSV
+			ArrayList<Mat> list = new ArrayList<Mat>();
+			Core.split(currentMatValue, list);
+			Mat vMat = null;
+			if (list.size() == 3)
+			{
+				vMat = list.get(0);
+				hArrayList.add(vMat);
+			}
+			else
+			{
+				System.out.println("Heyyyyyyy");
+				throw new RuntimeException("Image does not split to 3 channels!");
+			}
+		}
+		return hArrayList;
+	}
 	
 	
 	
