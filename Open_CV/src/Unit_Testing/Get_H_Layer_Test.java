cc5c0fe00e80c32541bb834c28f8e480a0915e81
Jean Carlo Mata <jean.cms07@gmail.com>
2016-08-20 21:44:44 -0600

    Unit testing functions

--- Open_CV/src/Unit_Testing/Get_H_Layer_Test.java
+++ Open_CV/src/Unit_Testing/Get_H_Layer_Test.java
@@ -2,13 +2,34 @@ package Unit_Testing;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+import org.opencv.core.Mat;
+
+import App.Video_Processing;
 
 public class Get_H_Layer_Test {
+	
+	@Test
+	// Verify ArrayList with values
+	public void getHlayerTestEmpty(){
+		Video_Processing video = new Video_Processing("Vid.mp4");
+		ArrayList<Mat> frames = video.readFrames();
+		ArrayList<Mat> hsvArrayList = video.convertToHSV(frames);
+		ArrayList<Mat> hArrayList = video.getHlayer(hsvArrayList);
+		assertFalse(hsvArrayList.isEmpty());
+	}
+	
 
 	@Test
-	public void getHlayerTest() {
-		fail("Not yet implemented");
+	public void getHlayerTestSize() {
+		Video_Processing video = new Video_Processing("Vid.mp4");
+		ArrayList<Mat> frames = video.readFrames();
+		ArrayList<Mat> hsvArrayList = video.convertToHSV(frames);
+		ArrayList<Mat> hArrayList = video.getHlayer(hsvArrayList);
+		int amountOfFrames = hArrayList.size();
+		assertEquals(1705, amountOfFrames);
 	}
 
 }
--- Open_CV/src/Unit_Testing/Read_Frames_Test.java
+++ Open_CV/src/Unit_Testing/Read_Frames_Test.java
@@ -3,6 +3,7 @@ package Unit_Testing;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.junit.Test;
 import org.opencv.core.Mat;
@@ -12,9 +13,20 @@ import App.Video_Processing;
 public class Read_Frames_Test {
 
 	@Test
-	public void readFramesTest() {
-		Video_Processing video = new Video_Processing("VidTest.mp4");
+	// Verify ArrayList with values
+	public void readFramesEmpty(){
+		Video_Processing video = new Video_Processing("Vid.mp4");
 		ArrayList<Mat> frames = video.readFrames();
+		assertFalse(frames.isEmpty());
+	}
+	
+	@Test
+	// Verify Amount of frames
+	public void readFramesTestSize() {
+		Video_Processing video = new Video_Processing("Vid.mp4");
+		ArrayList<Mat> frames = video.readFrames();
+		int amountOfFrames = frames.size();
+		assertEquals(1705, amountOfFrames);
 	}
 
 }
